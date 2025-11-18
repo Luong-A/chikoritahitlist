11f56c1c15cbab0c98c6fc4f5110d5216276b4b4
Luong-A <144722555+Luong-A@users.noreply.github.com>
2025-11-18 05:15:12 +0000

    init

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,39 +1,54 @@
 import { createFileRoute } from "@tanstack/react-router";
 import logo from "../logo.svg";
+import { useTRPC } from "@/lib/trpc-client";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { signInOptions } from "@/lib/auth-client";
+import { Plus } from "lucide-react";
 
 export const Route = createFileRoute("/")({
   component: App,
 });
 
 function App() {
+  const trpc = useTRPC();
+  const userData = useQuery(trpc.getUser.queryOptions());
+
+  const signIn = useMutation(signInOptions);
+
+  if (userData.isLoading) {
+    return (
+      <div>
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
+  if (!userData.data) {
+    return (
+      <div className="text-center ">
+        <h1 className="mt-5 pt-4 text-5xl">Chikorita HitList</h1>
+        <button
+          className="bg-amber-500 rounded-2xl border-b p-2 px-3"
+          onClick={() => signIn.mutateAsync()}
+        >
+          Sign In{" "}
+        </button>
+      </div>
+    );
+  }
   return (
-    <div className="text-center">
-      <header className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">
-        <img
-          src={logo}
-          className="h-[40vmin] pointer-events-none animate-[spin_20s_linear_infinite]"
-          alt="logo"
-        />
-        <p>
-          Edit <code>src/routes/index.tsx</code> and save to reload.
-        </p>
-        <a
-          className="text-[#61dafb] hover:underline"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <a
-          className="text-[#61dafb] hover:underline"
-          href="https://tanstack.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn TanStack
-        </a>
-      </header>
+    <div>
+      <h1 className="flex justify-center text-7xl m-2 "> Hit List</h1>
+      <h1 className="flex justify-center text-5xl m-2">Leaderboard</h1>
+      <h1 className="flex justify-center text-5xl m-2">Add Yours</h1>
+
+      <button
+        className="absolute right-10 bottom-10  
+flex justify-center text-2xl shadow-lg  rounded-full 
+h-12 w-12 items-center"
+      >
+        <Plus />
+      </button>
     </div>
   );
 }
--- src/server/trpc/routes/index.ts
+++ src/server/trpc/routes/index.ts
@@ -1,7 +1,12 @@
+import { extractAuth } from "../middleware/auth-middleware";
 import { publicProcedure, router } from "../trpc-config";
 
 export const appRouter = router({
   test: publicProcedure.query(async () => "Hi from the server!"),
+
+  getUser: publicProcedure
+    .use(extractAuth)
+    .query(({ ctx }) => ctx.user ?? null),
 });
 
 // Export type router type signature,
