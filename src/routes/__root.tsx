14977dfe1283b4b1857093c80c6ae9187bd3770a
SantiagoVira <santiagovira06@gmail.com>
2026-03-01 22:07:02 -0500

    title and favicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -27,7 +27,7 @@ export const Route = createRootRouteWithContext<RouterContext>()({
         content: "width=device-width, initial-scale=1",
       },
       {
-        title: "TanStack Start Starter",
+        title: "Chikorita Hit List",
       },
     ],
     links: [
